276a4270b4064745a31408f37678f170ff49b719
Paulo H <ph.hr.001@gmail.com>
2023-02-21 12:56:42 -0300

    - create the first implementation repository

---
+++ iburger/lib/app/repository/products/products_repository.dart
@@ -0,0 +1,5 @@
+import 'package:iburger/app/models/product_model.dart';
+
+abstract class ProductsRepository {
+  Future<List<ProductModel>> findAllProducts();
+}
---
+++ iburger/lib/app/repository/products/products_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:iburger/app/core/exception/repository_exception.dart';
+import 'package:iburger/app/core/rest_client/custom_dio.dart';
+import 'package:iburger/app/models/product_model.dart';
+import 'package:iburger/app/repository/products/products_repository.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  CustomDio dio;
+  ProductsRepositoryImpl(this.dio);
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final response = await dio.unAuth().get('/products');
+      final responseData = response.data;
+
+      List<ProductModel> listProducts = responseData
+          .map<ProductModel>((product) => ProductModel.fromJson(product))
+          .toList();
+
+      return listProducts;
+    } on DioError catch (e, s) {
+      const errorMessage = 'Erro ao buscar produtos';
+      log(errorMessage, error: errorMessage, stackTrace: s);
+      throw RepositoryException(message: errorMessage);
+    }
+  }
+}
