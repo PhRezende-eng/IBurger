976acbb580a866ee8eb1cdf44e7a5f2450877a67
Paulo H <ph.hr.001@gmail.com>
2023-02-21 12:54:27 -0300

    - create grandfather Provider, that is CustomDio

---
+++ iburger/lib/app/core/provider/application_binding.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:iburger/app/core/rest_client/custom_dio.dart';
+import 'package:provider/provider.dart';
+
+class ApplicationBinding extends StatelessWidget {
+  final Widget child;
+  const ApplicationBinding({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [Provider(create: (context) => CustomDio())],
+      child: child,
+    );
+  }
+}
