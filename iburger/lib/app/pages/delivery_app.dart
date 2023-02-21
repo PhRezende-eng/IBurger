5d7454e2b03f0060339d5e9d36be1142e87675b5
Paulo H <ph.hr.001@gmail.com>
2023-02-20 21:08:23 -0300

    - done splash screen

--- iburger/lib/app/pages/delivery_app.dart
+++ iburger/lib/app/pages/delivery_app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:iburger/app/core/ui/theme/theme_config.dart';
 import 'package:iburger/app/pages/splash/splash_page.dart';
 
--- iburger/lib/app/pages/splash/splash_page.dart
+++ iburger/lib/app/pages/splash/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:iburger/app/core/ui/helpers/size_extensions.dart';
+import 'package:iburger/app/core/ui/styles/colors_app.dart';
 import 'package:iburger/app/core/ui/widgets/delivery_button.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,19 +11,37 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Screen'),
-      ),
-      body: Column(
-        children: [
-          DeliveryButton(
-            label: 'Teste',
-            onPressed: () {},
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Text'),
-          ),
-        ],
+      body: ColoredBox(
+        color: ColorsApp.instance.splashBackgroundColor,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images.png ',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: context.percentHeight(0.3)),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 80),
+                  DeliveryButton(
+                    height: 35,
+                    width: context.percentWidth(0.6),
+                    label: 'ACESSAR',
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
