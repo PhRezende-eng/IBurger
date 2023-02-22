77c71b3e08262d1771345fd33f05a56fe01ea210
Paulo H <ph.hr.001@gmail.com>
2023-02-21 21:33:41 -0300

    - styling home page

--- iburger/lib/app/pages/home/home_page.dart
+++ iburger/lib/app/pages/home/home_page.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:iburger/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:iburger/app/models/product_model.dart';
+import 'package:iburger/app/pages/home/widgets/delivery_product_tile.dart';
+
+final product = ProductModel(
+  id: 0,
+  description: 'Description',
+  image:
+      'http://www.saboresajinomoto.com.br/uploads/images/recipes/sanduiche-de-churrasco.jpg',
+  name: 'Product name',
+  price: 5,
+);
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +23,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      appBar: DeliveryAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  DeliveryProductTile(
+                    key: ObjectKey(product.id),
+                    product: product,
+                  ),
+                  const Divider()
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
