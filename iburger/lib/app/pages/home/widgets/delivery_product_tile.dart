78fa4badb74b349c0b36a0d9d3966615e9561862
Paulo H <ph.hr.001@gmail.com>
2023-02-21 21:34:12 -0300

    - create widget for home page, product tile

---
+++ iburger/lib/app/pages/home/widgets/delivery_product_tile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:iburger/app/core/extensions/formatter_extension.dart';
+import 'package:iburger/app/core/ui/styles/colors_app.dart';
+import 'package:iburger/app/core/ui/styles/text_styles.dart';
+import 'package:iburger/app/models/product_model.dart';
+
+class DeliveryProductTile extends StatelessWidget {
+  final ProductModel product;
+  const DeliveryProductTile({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  product.name,
+                  style:
+                      context.textStyles.textExtraBold.copyWith(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  product.description,
+                  style: context.textStyles.textRegular.copyWith(fontSize: 14),
+                ),
+              ),
+              Text(
+                product.price.currencyPTBR,
+                style: context.textStyles.textMedium.copyWith(
+                  fontSize: 14,
+                  color: context.colors.secondary,
+                ),
+              ),
+            ],
+          ),
+          FadeInImage.assetNetwork(
+            placeholder: 'assets/images/loading.gif',
+            image: product.image,
+            height: 100,
+            width: 100,
+            fit: BoxFit.contain,
+          ),
+        ],
+      ),
+    );
+  }
+}
