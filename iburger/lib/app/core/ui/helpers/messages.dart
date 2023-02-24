a73185ad6e70d837f69ae838a800b747eaf7f3f9
Paulo H <ph.hr.001@gmail.com>
2023-02-24 00:05:16 -0300

    - create mixin to show on stateFul message by snack

---
+++ iburger/lib/app/core/ui/helpers/messages.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:iburger/app/core/ui/helpers/snack_bar_message.dart';
+
+mixin Messages<T extends StatefulWidget> on State<T> {
+  void showError(String message) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      DeliverySnackBar.error(context, message),
+    );
+  }
+
+  void showSuccess(String message) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      DeliverySnackBar.success(context, message),
+    );
+
+    // showTopSnackBar(
+    //   Overlay.of(context)!,
+    //   CustomSnackBar.success(message: message),
+    // );
+  }
+
+  void showInfo(String message) {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(
+      DeliverySnackBar.info(context, message),
+    );
+
+    // showTopSnackBar(
+    //   Overlay.of(context)!,
+    //   CustomSnackBar.info(message: message),
+    // );
+  }
+}
