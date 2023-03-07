7d76db576817074c3db36f31716a5da7904a6052
Paulo H <ph.hr.001@gmail.com>
2023-03-07 13:31:41 -0300

    - add loading state on loadProducts

--- iburger/lib/app/pages/home/home_controller.dart
+++ iburger/lib/app/pages/home/home_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:iburger/app/pages/home/home_state.dart';
 import 'package:iburger/app/repository/products/products_repository.dart';
@@ -8,9 +10,18 @@ class HomeController extends Cubit<HomeState> {
   HomeController(this._productsRepository) : super(const HomeState.initial());
 
   Future<void> loadProducts() async {
+    emit(state.copyWith(status: HomeStateStatus.loading));
     try {
       final products = await _productsRepository.loadProducts();
       emit(state.copyWith(status: HomeStateStatus.loaded, products: products));
-    } catch (e) {}
+    } catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      emit(
+        state.copyWith(
+          status: HomeStateStatus.error,
+          errorMessage: 'Error ao busca produtos',
+        ),
+      );
+    }
   }
 }
