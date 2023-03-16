a0ce7d34dede7807083d2678457dde35e7bdd2ea
Paulo H <ph.hr.001@gmail.com>
2023-03-16 00:35:38 -0300

    - add order product list on home state to manage the amount of product

--- iburger/lib/app/pages/home/home_state.dart
+++ iburger/lib/app/pages/home/home_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:iburger/app/dto/order_product_dto.dart';
 import 'package:iburger/app/models/product_model.dart';
 import 'package:match/match.dart';
 
@@ -15,18 +16,21 @@ enum HomeStateStatus {
 class HomeState extends Equatable {
   final HomeStateStatus status;
   final List<ProductModel> products;
+  //TODO: Could be change bellow type to MAP<String, OrderProductDto>, it will make query as O(1) which order transfer for next page (product_tile)
+  final List<OrderProductDto> shoppingBag;
   final String? errorMessage;
 
   const HomeState({
     required this.status,
     required this.products,
+    required this.shoppingBag,
     this.errorMessage,
   });
 
-  //could be static?
   const HomeState.initial()
       : status = HomeStateStatus.initial,
         products = const [],
+        shoppingBag = const [],
         errorMessage = null;
 
   @override
@@ -36,10 +40,12 @@ class HomeState extends Equatable {
   HomeState copyWith({
     HomeStateStatus? status,
     List<ProductModel>? products,
+    List<OrderProductDto>? shoppingBag,
     String? errorMessage,
   }) {
     return HomeState(
       errorMessage: errorMessage ?? this.errorMessage,
+      shoppingBag: shoppingBag ?? this.shoppingBag,
       status: status ?? this.status,
       products: products ?? this.products,
     );
