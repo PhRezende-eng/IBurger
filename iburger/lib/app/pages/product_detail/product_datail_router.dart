30a088857593b3865338a4f84fcd622cdacb43a4
Paulo H <ph.hr.001@gmail.com>
2023-03-16 00:37:17 -0300

    - receiving by args order product on router

--- iburger/lib/app/pages/product_detail/product_datail_router.dart
+++ iburger/lib/app/pages/product_detail/product_datail_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iburger/app/pages/product_detail/product_datail_page.dart';
+import 'package:iburger/app/pages/product_detail/product_detail_controller.dart';
 import 'package:provider/provider.dart';
 
 class ProductDetailRouter {
@@ -8,10 +9,18 @@ class ProductDetailRouter {
   static Widget get page => MultiProvider(
         providers: [
           Provider(
-            create: (context) => Object(),
+            create: (context) => ProductDetailController(),
           ),
           Provider.value(value: Object())
         ],
-        child: const ProductDetailPage(),
+        builder: (context, child) {
+          final args = ModalRoute.of(context)!.settings.arguments
+              as Map<String, dynamic>;
+
+          return ProductDetailPage(
+            product: args['product'],
+            order: args['order'],
+          );
+        },
       );
 }
