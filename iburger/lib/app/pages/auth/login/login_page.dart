4bf234d347e8fcb3e3ff7e65fb8c2399106aa3de
Paulo H <ph.hr.001@gmail.com>
2023-03-18 13:25:58 -0300

    - create new route page

--- iburger/lib/app/pages/auth/login/login_page.dart
+++ iburger/lib/app/pages/auth/login/login_page.dart
@@ -52,7 +52,9 @@ class LoginPage extends StatelessWidget {
                     style: context.textStyles.textBold,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/auth/register');
+                    },
                     child: Text(
                       "Cadastre-se",
                       style: context.textStyles.textBold
