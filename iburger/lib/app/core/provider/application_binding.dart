b454bf5c8c2e8732aaa6d777d7da4aa922bb9020
Paulo H <ph.hr.001@gmail.com>
2023-03-18 21:03:10 -0300

    add auth repo impl at bindings aplication

--- iburger/lib/app/core/provider/application_binding.dart
+++ iburger/lib/app/core/provider/application_binding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iburger/app/core/rest_client/custom_dio.dart';
+import 'package:iburger/app/repository/ayth/auth_repository.dart';
 import 'package:provider/provider.dart';
 
 class ApplicationBinding extends StatelessWidget {
@@ -13,7 +14,15 @@ class ApplicationBinding extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       //design partern creating singleton for dependecy ejection
-      providers: [Provider(create: (context) => CustomDio())],
+      providers: [
+        Provider(
+          create: (context) => CustomDio(),
+        ),
+        Provider<AuthRepositoryImpl>(
+          create: (context) =>
+              AuthRepositoryImpl(dio: context.read<CustomDio>()),
+        )
+      ],
       child: child,
     );
   }
