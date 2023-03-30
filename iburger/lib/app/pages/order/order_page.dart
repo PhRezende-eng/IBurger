f714521cc2326e07135a5754d11ba36fd7c48072
Paulo H <ph.hr.001@gmail.com>
2023-03-29 23:51:19 -0300

    - create order product tile to show on order screen with custom scroll view

--- iburger/lib/app/pages/order/order_page.dart
+++ iburger/lib/app/pages/order/order_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iburger/app/core/ui/styles/colors_app.dart';
+import 'package:iburger/app/core/ui/styles/text_styles.dart';
 import 'package:iburger/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:iburger/app/dto/order_product_dto.dart';
+import 'package:iburger/app/pages/order/widgets/order_product_tile.dart';
 
 class OrderPage extends StatelessWidget {
   final List<OrderProductDto> bag;
@@ -11,7 +14,41 @@ class OrderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppBar(),
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Row(
+                children: [
+                  Text(
+                    'Carrinho',
+                    style: context.textStyles.textTitle,
+                  ),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset('assets/images/trashRegular.png'),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: bag.length,
+              (context, index) {
+                return Column(
+                  children: [
+                    OrderProductTile(index, bag[index]),
+                    Divider(color: context.colors.borderSideButton),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ iburger/lib/app/pages/order/widgets/order_product_tile.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/widgets.dart';
+import 'package:iburger/app/core/extensions/formatter_extension.dart';
+import 'package:iburger/app/core/ui/styles/colors_app.dart';
+import 'package:iburger/app/core/ui/styles/text_styles.dart';
+import 'package:iburger/app/core/ui/widgets/delivery_increment_decrement_buttom.dart';
+import 'package:iburger/app/dto/order_product_dto.dart';
+
+class OrderProductTile extends StatelessWidget {
+  final int index;
+  final OrderProductDto orderProduct;
+  const OrderProductTile(this.index, this.orderProduct, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Image.network(
+            orderProduct.product.image,
+            height: 100,
+            width: 100,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  orderProduct.product.name,
+                  style: context.textStyles.textRegular.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      orderProduct.product.price.currencyPTBR,
+                      style: context.textStyles.textMedium.copyWith(
+                        fontSize: 14,
+                        color: context.colors.secondary,
+                      ),
+                    ),
+                    DeliveryIncrementDecrementButtom(
+                      amount: orderProduct.amount,
+                      incrementTap: () {},
+                      decrementTap: () {},
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
